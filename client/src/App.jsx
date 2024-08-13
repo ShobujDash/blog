fdc59f65d88a5df91d4ababb5893b99601d45b4c
ShobujDas <shobujd6.gmail.com>
2024-08-14 01:53:00 +0600

    [Done] responsive navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,19 +6,21 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SingUp';
 import Dashboard from './pages/DashBoard';
 import Projects from './pages/Projects';
+import Header from './components/Header';
 
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
         <Route path="/sign-in" element={<SignIn/>} />
         <Route path="/sign-up" element={<SignUp/>} />
         <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/project" element={<Projects/>} />
+        <Route path="/projects" element={<Projects/>} />
       </Routes>
     </BrowserRouter>
   );  
